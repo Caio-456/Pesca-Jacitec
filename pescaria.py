a7dd0ce2d1921ec0b4a35de9068507c908922c51
Caio Motta Barcelos <caiomottabarcelos13@gmail.com>
2025-09-17 21:15:15 -0300

    :sparkles:feat:Animacao e Particulas!

--- pescaria.py
+++ pescaria.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 from sys import exit
 
 # Pygame:
@@ -8,7 +9,7 @@ screen = pygame.display.set_mode((1000,625))
 pygame.display.set_caption('Pesca')
 clock = pygame.time.Clock()
 
-
+# Player:
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -21,7 +22,16 @@ class Player(pygame.sprite.Sprite):
         canoa7 = pygame.image.load('graficos/canoa/canoa7.png').convert_alpha()
         canoa8 = pygame.image.load('graficos/canoa/canoa8.png').convert_alpha()
         self.playerIndex = 0
-        self.playerAnimacao = [canoa1, canoa2, canoa3, canoa4, canoa5, canoa6, canoa7, canoa8]
+        # A lista tem repetidos para certos frames terem velocidades diferentes
+        self.playerAnimacao = [
+            canoa1, canoa1, 
+            canoa2, 
+            canoa3, canoa3, canoa3, canoa3, 
+            canoa4, 
+            canoa5, canoa5, 
+            canoa6, 
+            canoa7, canoa7, canoa7, canoa7, 
+            canoa8]
         self.imageOriginal = self.playerAnimacao[self.playerIndex]        
         self.image = self.imageOriginal
         self.anguloHistorico = [0,0,0,0,0,0,0,0,0,0]
@@ -38,65 +48,104 @@ class Player(pygame.sprite.Sprite):
             self.rect = self.image.get_rect(center = self.rect.center)
 
     def animacao(self):
-        if movendo is True:
-            self.playerIndex += 0.5
-            if self.playerIndex > 7:
-                self.playerIndex = 0
-            print(self.playerIndex)
-            self.imageOriginal = self.playerAnimacao[int(self.playerIndex)]
+        self.playerIndex += 0.3
+        if self.playerIndex > len(self.playerAnimacao):
+            self.playerIndex = 0
+        self.imageOriginal = self.playerAnimacao[int(self.playerIndex)]
+        self.image = pygame.transform.rotate(self.imageOriginal, -(self.anguloHistorico[-1]))
+        self.rect = self.image.get_rect(center = self.rect.center)
 
     def mover(self):
         global moverx
         global xPlayer
         global movery
         global yPlayer
+        global movendo
+        global rotacionar
         xPlayer = self.rect.centerx
         yPlayer = self.rect.centery
+        movendo = True
+        rotacionar = False
         self.rect.centerx = self.rect.centerx - moverx
         self.rect.centery = self.rect.centery - movery
-        
+        if self.rect.centerx == xPlayer and self.rect.centery == yPlayer:
+            movendo = False
+            rotacionar = True
+
+    def gerar_particulas(self, particulas):
+        for _ in range(2):
+            particulas.add(Particula(self.rect.center))
+
+
     def update(self):
-        if cursor_x >= 134 and cursor_y >= 80:
+        if cursor_x >= 134 and cursor_y >= 80 and rotacionar is True:
             self.rotacionar()
         self.mover()
-        if moverx != 0 and movery != 0:
+        if movendo:
             self.animacao()
+            player.sprite.gerar_particulas(particulas)
 
-# Player:
 player = pygame.sprite.GroupSingle()
 moverx = 0
 movery = 0
 movendo = False
-velocidade = 2
+velocidade = 3
 player.add(Player())
+rotacionar = True
+
+# Particulas
+class Particula(pygame.sprite.Sprite):
+    def __init__(self, posicao):
+        super().__init__()
+        self.image = pygame.Surface((3, 2), pygame.SRCALPHA) # SRCALPHA significa que os pixels terão um canal alpha(transparência)
+        pygame.draw.circle(self.image, (143, 211, 255), (2, 2), 3) # cor, centro, raio
+        self.rect = self.image.get_rect(center = posicao)
+        self.moverX = random.uniform(-1, 3) # uniform é quando aceita float
+        self.moverY = random.uniform(1, 3)
+        self.timer = 30
+
+    def update(self):
+        self.rect.x += self.moverX
+        self.rect.y += self.moverY
+        self.timer -= 1
+        opacidade = self.timer / 40
+        self.image.set_alpha(abs(int(255 * opacidade)))
+        if self.timer <= 0:
+            self.kill()
+
+particulas = pygame.sprite.Group()
 
 # Background:
 background = pygame.image.load('graficos/background.png').convert()
 
-while True:
+# Eventos pygame:
+def processarEventos():
+    global click_x, click_y
+    global rotacionar
+    global moverx, movery
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                click_x, click_y = pygame.mouse.get_pos()
-                if cursor_x >= 134 and cursor_y >= 80:
-                    if event.button == 1:
-                        print("Left click detected!")
-                        movendo = True
-                        if xPlayer < click_x:
-                            moverx = -velocidade
-                        if xPlayer > click_x:
-                            moverx = velocidade
-                        if yPlayer < click_y:
-                            movery = -velocidade
-                        if yPlayer > click_y:
-                            movery = velocidade
-                    elif event.button == 3:
-                        print("Right click detected!")
-                    print(click_x, click_y)
+            click_x, click_y = pygame.mouse.get_pos()
+            if cursor_x >= 134 and cursor_y >= 80:
+                rotacionar = True
+                if event.button == 1:
+                    if xPlayer < click_x:
+                        moverx = -velocidade
+                    if xPlayer > click_x:
+                        moverx = velocidade
+                    if yPlayer < click_y:
+                        movery = -velocidade
+                    if yPlayer > click_y:
+                        movery = velocidade
+                elif event.button == 3:
+                    print("Botão direito!")
+
+while True:
+    processarEventos()
 
     cursor_x, cursor_y = pygame.mouse.get_pos()
     if cursor_x < 134 or cursor_y < 80:
@@ -110,10 +159,14 @@ while True:
         if abs(yPlayer - click_y) < 10:
             movery = 0 
 
+    # Updates:
     player.update()
+    particulas.update()
+
+    # Desenhar:
     screen.blit(background, (0,0))
+    particulas.draw(screen)
     player.draw(screen)
 
-    pygame.display.flip()
     pygame.display.update()
     clock.tick(60)
